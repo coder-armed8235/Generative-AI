d625db845a432aa6418dc5e104c6fd3376531a51
Amit Kumar <www.yadavanshiamit@gmail.com>
2026-07-25 15:13:11 +0530

    tools creation

--- Day_03/index.js
+++ Day_03/index.js
@@ -1,3 +1,7 @@
+import { GoogleGenAI } from '@google/genai';
+import readlineSync from 'readline-sync';
+import 'dotenv/config'
+const client = new GoogleGenAI({});
 
 async function Cryptocurrency({coin}){
     const response = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&ids=${coin}`);
@@ -7,9 +11,39 @@ async function Cryptocurrency({coin}){
     return data;
 }
 
-async function WeatherInfromation({city}){
+async function WeatherInformation({city}){
     const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=aacecf3694874beea8a165544260804&q=${city}&aqi=no`);
     const data = await response.json();
 
     console.log(data);
+}
+
+const CryptoInfo = {
+    name:'Cryptocurrency',
+    description:'Gets the current crypto detail for a given coins.',
+    parameters:{
+        type:'object',
+        properties:{
+            coin:{
+                type:'string',
+                description:'The coin'
+            }
+        },
+        required:['coin'],
+    }
+}
+
+const WeatherInfo = {
+    name:'WeatherInformation',
+    description:'Gets the current weather temperature for a given location.',
+    parameters:{
+        type:'object',
+        properties:{
+            coin:{
+                type:'string',
+                description:'The coin'
+            }
+        },
+        required:['coin'],
+    }
 }
